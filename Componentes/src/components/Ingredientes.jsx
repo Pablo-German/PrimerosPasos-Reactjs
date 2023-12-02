0af5479a810858348b052e2f4be27c27d7196b49
Pablo-German <pabloajallaenero26@gmail.com>
2023-12-02 01:23:20 -0300

    w

--- Componentes/src/components/Ingredientes.jsx
+++ Componentes/src/components/Ingredientes.jsx
@@ -1,11 +1,12 @@
 
 function Ingredientes(title,ingre) {
+    
     return ( 
         <section>
         <h2>{title}</h2>
         <ul>
         {ingre.map((ing,idx)=>(
-            <li key={idx}>{ing}</li>
+            <li key={idx}>{ing.name} - {ing.amount} {ing.measurement}</li>
         ))}
         </ul>
         
